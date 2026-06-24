24be1c79ba93cbe31fc3e8f72d523a38ed0b4a53
Steph Crown <emmanuelstephen024@gmail.com>
2026-06-25 00:43:54 +0100

    Update 02.sh command

--- submission/02.sh
+++ submission/02.sh
@@ -1,2 +1,2 @@
 # Write the bitcoin cli command to get the bitcoin node network name
-bitcoin-cli -regtest getblockchaininfo
+bitcoin-cli -regtest getblockchaininfo | grep '"chain":' | cut -d'"' -f4
