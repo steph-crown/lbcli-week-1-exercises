4b6a4ee760417e30a8a975b76969f065ff0f29ae
Steph Crown <emmanuelstephen024@gmail.com>
2026-06-25 08:51:23 +0100

    Update final ssubmission

--- submission/final.sh
+++ submission/final.sh
@@ -175,7 +175,7 @@ check_cmd "Getting address info"
 
 # STUDENT TASK: Extract the internal key (the x-only pubkey) from the descriptor
 # WRITE YOUR SOLUTION BELOW:
-INTERNAL_KEY=$(echo "$ADDR_INFO" | grep -o '"internal_pubkey": "[^"]*"' | cut -d'"' -f4)
+INTERNAL_KEY=$(echo "$ADDR_INFO" | grep -o '"pubkey": "[^"]*"' | cut -d'"' -f4)
 check_cmd "Extracting key from descriptor"
 INTERNAL_KEY=$(trim "$INTERNAL_KEY")
 
