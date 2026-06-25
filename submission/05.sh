b370273be1529d3d902d36795d62017d76591521
Steph Crown <emmanuelstephen024@gmail.com>
2026-06-25 08:09:33 +0100

    Update 05 submision to mine blocks before checking total amount

--- submission/05.sh
+++ submission/05.sh
@@ -1,3 +1,5 @@
 # Check the total amount in the wallet.
 # bitcoin-cli -regtest getbalances | grep '"trusted":' | cut -d'"' -f3 | cut -d':' -f2 | xargs
+ADDR=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress)
+bitcoin-cli -regtest generatetoaddress 101 "$ADDR" > /dev/null
 bitcoin-cli -regtest -rpcwallet=builderswallet getbalance
